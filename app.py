ccf2f44cab8cae565830ff4a183c90667450bb54
dawun <dawun@lacy.co.kr>
2024-02-10 15:31:19 +0900

    STI 개발 완료

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, Response
+
+load_dotenv(verbose=True)
+
+client = OpenAI()
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/", methods=["GET"])
+def index():
+    return render_template('index.html')
+
+
+@app.route("/textToImage", methods=["POST"])
+def textToImage():
+    prompt = request.form.get('prompt')
+
+    dallEResponse = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    return Response(
+        json.dumps({
+            "imageUrl": dallEResponse.data[0].url
+        }),
+        status=200,
+        mimetype="application/json"
+    )
 
 
 if __name__ == '__main__':
---
+++ static/js/index.js
@@ -0,0 +1,80 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var diagnosticPara = document.querySelector('.output');
+
+const recognition = new SpeechRecognition();
+const speechRecognitionList = new SpeechGrammarList();
+
+let promptList = [];
+let stopSpeak = false;
+
+function sendSpeech() {
+    recognition.grammars = speechRecognitionList;
+    recognition.lang = 'ko-KR';
+    recognition.interimResults = false;
+    recognition.continious = true;
+    recognition.maxAlternatives = 1;
+
+    recognition.start();
+
+    recognition.onresult = function (event) {
+        var speechResult = event.results[0][0].transcript.toLowerCase();
+
+        // 중간에 말하기를 멈추기 위한 커맨드
+        if (speechResult === '여기까지') {
+            stopSpeak = true;
+        } else {
+            $('#sttContent').text($('#sttContent').text() + speechResult + '\n');
+            promptList.push(speechResult);
+        }
+    }
+
+    recognition.onend = function (event) {
+        // 한문장이 끝난 뒤 종료 되기에 다음 문장을 받기 위해 이어서 시작해준다.
+        if (!stopSpeak && promptList.length < 10) {
+            recognition.start();
+        }
+    }
+}
+
+$('#speachIdea').on('click', function () {
+    // 음성 인식 시작
+    sendSpeech();
+
+    // 버튼 전환
+    $('#speachIdea').hide();
+    $('#resetPage').show();
+    $('#createImage').show();
+});
+
+$('#resetPage').on('click', function () {
+    // 새로고침
+    location.reload();
+});
+
+$('#createImage').on('click', function () {
+    $('#createImage').hide();
+    $('#sttContainer').hide();
+    $('#loadingContainer').show();
+
+    let promptString = '';
+    promptList.forEach((string) => {
+        promptString += (string + '\n');
+    });
+
+    const data = {
+        prompt: promptString
+    };
+
+    $.ajax({
+        type: 'post',
+        url: '/textToImage',
+        data: data,
+        dataType: 'json',
+        success: function (result) {
+            $('#dallEImage').attr("src", result.imageUrl);
+            $('#loadingContainer').hide();
+            $('#dallEResponseContainer').show();
+        }
+    });
+});
